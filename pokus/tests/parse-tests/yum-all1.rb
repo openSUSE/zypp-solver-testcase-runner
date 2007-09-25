163a6d2dee9f55fd66f08674f086313071531aa9
Arvin Schnell <aschnell@suse.de>
2007-09-25 12:41:58 +0000

    - added two tests

---
+++ pokus/tests/parse-tests/yum-all1.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/ruby
+
+
+# Check handling of various package data.
+
+
+require 'zypptools/lib/package'
+require 'zypptools/output/output'
+include ZYppTools
+
+require '../../src/testsuite'
+
+
+packages = Array.new()
+
+pkg = Package.new("test")
+pkg.arch = "i586"
+pkg.version = "1.2"
+pkg.release = 1
+pkg.installsize = 84000
+pkg.rpmsize = 42000
+pkg.summary = "A pure test package"
+packages.push(pkg)
+
+
+path = Testsuite::write_repo(:yum, packages)
+
+
+Testsuite.set_arch("i586")
+pool = Testsuite.read_repo("file://" + path)
+
+
+Testsuite::haha2(pool).each do |res|
+    puts "#{res.kind_to_s} #{res.name} #{res.edition.to_s} #{res.arch.to_s}"
+    puts "  Summary: #{res.summary}"
+    puts "  Size: #{res.size} / #{res.download_size}"
+    Testsuite::dump_all_deps(res)
+    puts
+end
+
