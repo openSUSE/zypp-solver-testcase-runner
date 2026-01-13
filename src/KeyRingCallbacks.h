da52ca503fcf4e132916f6c0454e45cd1c4093c1
Benjamin Zeller <bzeller@suse.de>
2026-01-13 12:33:32 +0100

    Remove legacy includes

--- src/KeyRingCallbacks.h
+++ src/KeyRingCallbacks.h
@@ -15,9 +15,9 @@
 
 #include <iostream>
 
-#include <zypp/base/Logger.h>
+#include <zypp-core/base/Logger.h>
 #include <zypp/ZYppCallbacks.h>
-#include <zypp/Pathname.h>
+#include <zypp-core/Pathname.h>
 #include <zypp/KeyRing.h>
 #include <zypp/Digest.h>
 
